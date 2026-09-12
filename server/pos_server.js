8117862cfa9dc02895788523d807173514d8d3f0
MahadevEnterpeise <mahadev100enterprise@gmail.com>
2026-09-12 07:51:47 +0000

    link changed

--- server/pos_server.js
+++ server/pos_server.js
@@ -690,7 +690,7 @@ router.post('/user-me', async (req, res) => {
         const { uid, shopId } = req.body;
         if (!uid || !shopId) return res.status(400).json({ error: 'Missing parameters' });
 
-        const qrUrl = `https://kineticpos-production.up.railway.app//auth?next=${encodeURIComponent(`/order/${shopId}`)}`;
+        const qrUrl = `https://kineticpos-production.up.railway.app/auth?next=${encodeURIComponent(`/order/${shopId}`)}`;
         res.status(200).json({ url: qrUrl });
     } catch (error) {
         console.error('❌ Error generating QR landing path:', error.message);
