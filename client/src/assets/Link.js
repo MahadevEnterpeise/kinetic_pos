db3d0d2bb694afeb5db247c6756ac13fd821e0e5
MahadevEnterpeise <mahadev100enterprise@gmail.com>
2026-09-12 08:06:45 +0000

    link changed

--- client/src/assets/Link.js
+++ client/src/assets/Link.js
@@ -1,6 +1,6 @@
 //export const link='http://localhost:3000/api'
 //now 
-export const link ='/api'
+export const link ='https://kineticpos-production.up.railway.app/api'
 //export const link = `https://${window.location.hostname.replace('-5173', '-3000')}/api`;
 
 
