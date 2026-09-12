76001791d30073583d894a846fcf987aa5ea0de8
MahadevEnterpeise <mahadev100enterprise@gmail.com>
2026-09-12 07:28:14 +0000

    added path slashline45

--- server/pos_server.js
+++ server/pos_server.js
@@ -42,7 +42,7 @@ app.use(express.json());
 // Serve Vue frontend static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 // HTTP Server & WebSocket Subsystem Integration
