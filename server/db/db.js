e8075763538d890796826a42069ebc61a0e1b732
MahadevEnterpeise <mahadev100enterprise@gmail.com>
2026-09-25 11:36:23 +0530

    Update db.js
    logta

--- server/db/db.js
+++ server/db/db.js
@@ -142,7 +142,7 @@ console.log('✅ feedback table verified/created successfully.');
 
 // 7. Create audit Table
 await db.query(`
-CREATE TABLE IF NOT EXISTS audit_logs (
+CREATE TABLE IF NOT EXISTS audit_log (
 id INT AUTO_INCREMENT PRIMARY KEY,
 shop_id VARCHAR(50) NOT NULL,
 actor_name VARCHAR(100) NOT NULL,
